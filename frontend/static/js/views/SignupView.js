9b6086adb72f932a27f84be4a4f953cdc49753e7
adaken4 <adakennedy@outlook.com>
2025-03-18 09:44:10 +0300

    [feat]: enable toggling password visibility on or off

--- frontend/static/js/views/SignupView.js
+++ frontend/static/js/views/SignupView.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import AbstractView from "./AbstractView.js";
 
 export default class SignupView extends AbstractView {
@@ -8,21 +10,21 @@ export default class SignupView extends AbstractView {
 
   async getHtml() {
     return `
-            <form action="/login" method="post" autocomplete="off" class="form">
+            <form id="signupForm" action="/signup" method="post" autocomplete="off" class="form">
               <p class="title">Register</p>
               <p class="message">Signup now and get full access to our app.</p>
               <div class="form-group">
                 <label for="">
-                  <input type="text" required />
+                  <input id="firstName" type="text" required />
                   <span>Firstname</span>
                 </label>
                 <label for="">
-                  <input type="text" required />
+                  <input id="lastName" type="text" required />
                   <span>Lastname</span>
                 </label>
               </div>
               <label for="">
-                <input type="text" required />
+                <input id="email" type="text" required />
                 <span>Email</span>
               </label>
               <label for="">
@@ -40,13 +42,29 @@ export default class SignupView extends AbstractView {
                 </span>
               </label>
               <button class="submit">Submit</button>
-              <p class="signin">Already have an account ? <a href="/login" data-link>Signin</a></p>
+              <p class="signin">Already have an account ? <a href="/signin" data-link>Signin</a></p>
             </form>
         `;
   }
 
   async onMounted() {
     const form = document.getElementById("signupForm");
+
+    const passwordInput = document.getElementById("password");
+    const togglePassword = document.getElementById("togglePassword");
+    const passwordConfirm = document.getElementById("passwordConfirm");
+    const togglePasswordConfirm = document.getElementById(
+      "togglePasswordConfirm"
+    );
+
+    togglePassword.addEventListener("click", () => {
+      this.togglePasswordVisibility(passwordInput, togglePassword);
+    });
+
+    togglePasswordConfirm.addEventListener("click", () => {
+      this.togglePasswordVisibility(passwordConfirm, togglePasswordConfirm);
+    });
+
     form.addEventListener("submit", async (event) => {
       event.preventDefault();
 
@@ -77,5 +95,15 @@ export default class SignupView extends AbstractView {
         alert("Something went wrong.");
       }
     });
+  }
+
+  togglePasswordVisibility(inputElement, toggleElement) {
+    if (inputElement.type === "password") {
+      inputElement.type = "text";
+      toggleElement.innerHTML = '<i class="far fa-eye"></i>';
+    } else {
+      inputElement.type = "password";
+      toggleElement.innerHTML = '<i class="far fa-eye-slash"></i>';
+    }
   }
 }
