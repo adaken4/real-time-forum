b187eaa310375e4d5fd6387a36108888bef05068
adaken4 <adakennedy@outlook.com>
2025-11-27 20:06:42 +0300

    feat: implement Post and PostDetail structs for forum posts management (#2)

--- internal/domain/post.go
+++ internal/domain/post.go
@@ -1,1 +1,23 @@
 package domain
+
+import "time"
+
+type Post struct {
+	ID           int
+	UserID       int
+	Author       string
+	Title        string
+	Content      string
+	Category     string
+	FileURL      *string
+	CommentCount int
+	LikeCount    int
+	DislikeCount int
+	UserReaction *string // 'like', 'dislike', or null
+	CreatedAt    time.Time
+}
+
+type PostDetail struct {
+	Post     Post
+	Comments []Comment
+}
