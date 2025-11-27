35ae30f31cc54e521aa92f165480625835c4455d
adaken4 <adakennedy@outlook.com>
2025-11-27 20:05:17 +0300

    feat: define Message struct for managing forum messages (#2)

--- internal/domain/message.go
+++ internal/domain/message.go
@@ -1,1 +1,13 @@
 package domain
+
+import "time"
+
+type Message struct {
+	ID         int
+	FromUserID int
+	ToUserID   int
+	Content    string
+	FileURL    *string
+	ReadStatus bool
+	CreatedAt  time.Time
+}
