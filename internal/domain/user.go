8cffb2974cb24c6a958b233fd8959269e2343f19
adaken4 <adakennedy@outlook.com>
2025-11-27 20:03:23 +0300

    feat: define User and UserStatus structs for user management (#2)

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,1 +1,24 @@
 package domain
+
+import "time"
+
+type User struct {
+	ID           int
+	Email        string
+	Nickname     string
+	PasswordHash string
+	FirstName    string
+	LastName     string
+	Age          int
+	Gender       string
+	CreatedAt    time.Time
+}
+
+type UserStatus struct {
+	ID        int
+	UserID    int
+	Nickname  string
+	Online    bool
+	LastSeen  time.Time
+	UpdatedAt time.Time
+}
