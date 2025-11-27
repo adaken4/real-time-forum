0f87cf03e1cc76bd36e7b87cfaf8af231c68fd46
adaken4 <adakennedy@outlook.com>
2025-11-27 20:02:09 +0300

    feat: define ReactionType and Reaction struct for user interactions (#2)

--- internal/domain/reaction.go
+++ internal/domain/reaction.go
@@ -1,1 +1,16 @@
 package domain
+
+type ReactionType string
+
+const (
+	Like    ReactionType = "like"
+	Dislike ReactionType = "dislike"
+	Remove  ReactionType = "remove"
+)
+
+type Reaction struct {
+	Success      bool
+	LikeCount    int
+	DislikeCount int
+	UserReaction *string
+}
